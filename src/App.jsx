0581e140588f512bf04f9ae62021aab8d551b74c
Safdar Jamal <cleverprogrammer.me@gmail.com>
2019-04-20 19:57:03 +0500

    Data fetched and logged in console

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const DEFAULT_QUERY = 'redux';
+
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+console.log(url);
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      result: null,
+      searchTerm: DEFAULT_QUERY
+    };
+
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
+  }
+
+  setSearchTopStories(result) {
+    this.setState({ result });
+  }
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error);
+  }
+
+  render() {
+    const { result, searchTerm } = this.state;
+    console.log(result);
+    if (!result) {
+      // console.log('bingo', !result);
+      return null;
+    }
+    return (
+      <div className="App page">
+        <h1>Hacker News</h1>
+        <a href={result.hits[1].url}>Story 1</a>
+      </div>
+    );
+  }
+}
+
+export default App;
